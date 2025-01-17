652cf74a90d87e1aaf3e72f57da7dbed7112e04d
wuhaohao <1611499758@qq.com>
2025-01-17 22:56:49 +0800

    feat: 添加

--- vite.config.js
+++ vite.config.js
@@ -9,36 +9,25 @@ export default defineConfig({
     outDir: 'docs',
     rollupOptions: {
       output: {
-        // 确保所有文件都在 assets 目录下
-        assetFileNames: (assetInfo) => {
-          const info = assetInfo.name.split('.')
-          let extType = info[info.length - 1]
-          if (/\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/i.test(assetInfo.name)) {
-            extType = 'media'
-          } else if (/\.(png|jpe?g|gif|svg|bmp|webp)(\?.*)?$/i.test(assetInfo.name)) {
-            extType = 'img'
-          } else if (/\.(woff2?|eot|ttf|otf)(\?.*)?$/i.test(assetInfo.name)) {
-            extType = 'fonts'
+        // 简化文件名
+        assetFileNames: 'assets/[name].[hash:8].[ext]',
+        chunkFileNames: 'js/[name].[hash:8].js',
+        entryFileNames: 'js/[name].[hash:8].js',
+        // 简化代码分割
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('vue')) {
+              return 'vue'
+            }
+            if (id.includes('ant-design-vue')) {
+              return 'antd'
+            }
+            return 'vendor'
           }
-          return `assets/${extType}/[name]-[hash][extname]`
-        },
-        chunkFileNames: 'assets/js/[name]-[hash].js',
-        entryFileNames: 'assets/js/[name]-[hash].js',
-        // 添加这个配置来处理代码分割
-        manualChunks: {
-          'vue': ['vue'],
-          'vue-router': ['vue-router'],
-          'ant-design-vue': ['ant-design-vue'],
-          'vendor': ['@ant-design/icons-vue']
         }
       }
     },
-    // 使用 esbuild 压缩
     minify: 'esbuild',
-    sourcemap: false,
-    // 确保正确处理静态资源
-    assetsInlineLimit: 4096,
-    cssCodeSplit: true,
-    modulePreload: true
+    sourcemap: false
   }
 })
